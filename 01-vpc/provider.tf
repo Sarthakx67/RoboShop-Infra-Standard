3fb437ada64fd26e8c890fd034c548d46977a5a7
sarthak <sarthakx67@gmail.com>
2025-08-17 21:36:51 +0530

    update

--- 01-vpc/provider.tf
+++ 01-vpc/provider.tf
@@ -5,12 +5,12 @@ terraform {
       version = "~> 6.0"
     }
   }
-  backend "s3" {
-    bucket   = "sarthak-remote-tfstate"
-    key = "vpc-roboshop"
-    region = "ap-south-1"
-    dynamodb_table = "sarthak-tfstate-lock"
-  }
+  # backend "s3" {
+  #   bucket   = "sarthak-remote-tfstate"
+  #   key = "vpc-roboshop"
+  #   region = "ap-south-1"
+  #   dynamodb_table = "sarthak-tfstate-lock"
+  # }
 }
 
 provider "aws" {
